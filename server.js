94a6c19d12697e1b31b033f411df845e1159727c
ankurr7 <godaseankur@gmail.com>
2026-04-10 22:28:20 +0530

    deploy fix

--- server.js
+++ server.js
@@ -40,4 +40,5 @@ app.post('/search', (req, res) => {
     }
 });
 
-app.listen(3000, () => console.log("Running on http://localhost:3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running"));
